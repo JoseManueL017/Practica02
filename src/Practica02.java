31f0d8769662f13fe657893e26fe624e452bc0c8
Josema017 <jmom1991@gmail.com>
2022-01-21 17:23:53 +0100

    septimo commit

--- src/Practica02.java
+++ src/Practica02.java
@@ -3,7 +3,7 @@ public class Practica02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World! parte 4");
+		System.out.println("Hello World! parte 5");
 
 	}
 
